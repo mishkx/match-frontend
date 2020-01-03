1873f927f3ca4f21c10c7501f48255b42f3827d9
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-01-03 10:38:14 +0300

    Добавить react-hot-loader

--- src/root.tsx
+++ src/root.tsx
@@ -1,3 +1,4 @@
+import { hot } from 'react-hot-loader/root';
 import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import { Provider } from 'react-redux';
@@ -18,4 +19,4 @@ const Root: React.FC = () => (
     </Provider>
 );
 
-export default Root;
+export default hot(Root);
