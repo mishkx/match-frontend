56d4172e1881320f40c1c39c7e977932eb31d3cf
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-01-05 19:46:41 +0300

    Использовать дженерики для actions

--- src/actions/helpers.ts
+++ src/actions/helpers.ts
@@ -1,23 +1,36 @@
 import {
-    RequestTypes,
-    CreateRequestTypes,
-    Action,
-    ActionType,
-    ActionPayload,
+    ActionCreator,
     ActionObject,
+    ActionPayload,
+    ActionType,
+    CreateRequestTypes,
+    RequestTypes,
 } from './types';
 
-export const action: Action = (type: ActionType, payload?: ActionPayload): ActionObject => {
-    return {
-        type,
-        payload,
+export class Action<T = ActionPayload> implements ActionCreator {
+    constructor(protected _type: ActionType, protected _payload?: T) {}
+    get type(): ActionType {
+        return this._type;
     }
-};
+    get payload(): T | undefined {
+        return this._payload;
+    }
+    setPayload(payload: T): this {
+        this._payload = payload;
+        return this;
+    }
+    get action(): ActionObject<T> {
+        return {
+            type: this.type,
+            payload: this.payload,
+        };
+    }
+}
 
 export const createRequestTypes: CreateRequestTypes = (base: string): Record<RequestTypes, ActionType> => {
     return {
         [RequestTypes.REQUEST]: `${base}_${RequestTypes.REQUEST}`,
         [RequestTypes.SUCCESS]: `${base}_${RequestTypes.SUCCESS}`,
-        [RequestTypes.FAILURE]: `${base}_${RequestTypes.FAILURE}`,
+        [RequestTypes.ERROR]: `${base}_${RequestTypes.ERROR}`,
     };
 };
--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,24 +1,40 @@
-export enum RequestTypes {
-    REQUEST = 'REQUEST',
-    SUCCESS = 'SUCCESS',
-    FAILURE = 'FAILURE',
-}
+import { AxiosResponse } from 'axios';
+import { ErrorData } from '../api/types';
 
 export type ActionType = string;
 
 export type ActionPayload = any;
 
-export interface ActionObject {
+export type ActionObject<T = ActionPayload> = {
     type: ActionType;
-    payload?: ActionPayload;
+    payload?: T;
 }
 
-export type Action = (type: ActionType, payload?: ActionPayload) => ActionObject;
+export interface ActionCreator<T = ActionPayload> {
+    readonly type: ActionType;
+    readonly payload?: T;
+    readonly action: ActionObject<T>;
+    setPayload(payload: T): this;
+}
+
+export interface RequestAction<S = ActionPayload, E = ErrorData> {
+    request: ActionCreator;
+    success: ActionCreator<S>;
+    error: ActionCreator<E>;
+}
+
+export enum RequestTypes {
+    REQUEST = 'REQUEST',
+    SUCCESS = 'SUCCESS',
+    ERROR = 'ERROR',
+}
 
 export type CreateRequestTypes = (base: string) => Record<RequestTypes, ActionType>;
 
-export interface RequestAction {
-    request: (...args: any) => ActionObject;
-    success: (...args: any) => ActionObject;
-    failure: (...args: any) => ActionObject;
+export type ErrorResponseData = {
+    exception?: string,
+    errors?: [string[]],
+    message?: string,
 }
+
+export type ErrorResponse = AxiosResponse<ErrorResponseData>;
