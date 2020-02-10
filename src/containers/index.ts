c3170873f789434de0df363497409ca7504ad2c6
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 02:06:13 +0300

    Добавить общий экспорт контейнеров

---
+++ src/containers/index.ts
@@ -0,0 +1,33 @@
+import AppContainer from './App';
+import ChatFormContainer from './ChatForm';
+import ChatsCollectionContainer from './ChatsCollection';
+import ErrorBoundary from './ErrorBoundary';
+import InitialContainer from './Initial';
+import MatchesChatsContainer from './MatchesChats';
+import MatchesCollectionContainer from './MatchesCollection';
+import NotFoundContainer from './NotFound';
+import ProfileContainer from './Profile';
+import RecommendationsContainer from './Recommendation';
+import SettingsContainer from './Settings';
+import SettingsPhotosContainer from './SettingsPhotos';
+import SingleChatContainer from './SingleChat';
+import SingleMatchContainer from './SingleMatch';
+import SocketContainer from './Socket';
+
+export {
+    AppContainer,
+    ChatFormContainer,
+    ChatsCollectionContainer,
+    ErrorBoundary,
+    InitialContainer,
+    MatchesChatsContainer,
+    MatchesCollectionContainer,
+    NotFoundContainer,
+    ProfileContainer,
+    RecommendationsContainer,
+    SettingsContainer,
+    SettingsPhotosContainer,
+    SingleChatContainer,
+    SingleMatchContainer,
+    SocketContainer,
+}
