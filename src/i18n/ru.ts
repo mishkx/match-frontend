8a9ba056b364c10218c7ed6f1481c843ca279b2c
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 01:22:27 +0300

    Добавить локализацию ru

---
+++ src/i18n/ru.ts
@@ -0,0 +1,31 @@
+import { ResourceLanguage } from './types';
+
+const ru: ResourceLanguage = {
+    translation: {
+        '{{distance}} km from you': '{{distance}} км от вас',
+        'Add photo': 'Добавить фото',
+        Age: 'Возраст',
+        'Are you sure?': 'Вы уверены?',
+        Contact: 'Написать',
+        'Delete match': 'Удалить из пар',
+        Distance: 'Расстояние',
+        Female: 'Женский',
+        'Go to recommendations': 'В рекомендации',
+        'Go to settings': 'В настройки',
+        'It\'s a match!': 'Новая пара!',
+        'Keep searching': 'Продолжить поиски',
+        Male: 'Мужской',
+        'Page not found': 'Страница не найдена',
+        Photos: 'Фотографии',
+        Required: 'Обязательно',
+        Save: 'Сохранить',
+        Searching: 'Поиск',
+        Settings: 'Настройки',
+        'Successfully updated': 'Успешно обновлено',
+        'There is no one near you': 'Рядом с вами никого нет',
+        'Try to change your search options': 'Попробуйте изменить параметры поиска',
+        'Update profile': 'Изменить информацию',
+    },
+};
+
+export default ru;
