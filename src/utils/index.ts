d50738037695c3e0b8c9f9be7f195047c4395cc1
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 01:17:26 +0300

    Добавить общий экспорт хелперов

---
+++ src/utils/index.ts
@@ -0,0 +1,17 @@
+import history from './history';
+import moment from './moment';
+import request from './request';
+import Socket from './socket';
+import store from './store';
+
+export * from './helpers';
+export * from './request';
+export * from './socket';
+
+export {
+    history,
+    moment,
+    request,
+    Socket,
+    store,
+};
