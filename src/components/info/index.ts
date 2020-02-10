759bc9fe9005a917f4aac5799429025933eab3de
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 01:45:03 +0300

    Добавить общий экспорт в информационные компоненты

---
+++ src/components/info/index.ts
@@ -0,0 +1,11 @@
+import AppLoading from './AppLoading';
+import NoRecommendations from './NoRecommendations';
+import NotFound from './NotFound';
+import RecommendationsLoading from './RecommendationsLoading';
+
+export {
+    AppLoading,
+    NoRecommendations,
+    NotFound,
+    RecommendationsLoading,
+}
