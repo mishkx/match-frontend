c06478374a31f974445b7e9f1b7df623031528d8
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 02:12:48 +0300

    Добавить кастомные настройки в react-app-rewire

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,21 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const { override, addLessLoader } = require('customize-cra');
 
-/* config-overrides.js */
-module.exports = function override(config, env) {
-    config = rewireReactHotLoader(config, env);
+// /* config-overrides.js */
+module.exports = override(
+    function override(config, env) {
+        config = rewireReactHotLoader(config, env);
 
-    config.resolve.alias = {
-        ...config.resolve.alias,
-        'react-dom': '@hot-loader/react-dom',
-    };
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'react-dom': '@hot-loader/react-dom',
+        };
 
-    return config;
-};
+        config.optimization.splitChunks = false;
+
+        return config;
+    },
+    addLessLoader({
+        javascriptEnabled: true,
+    }),
+);
