ed1fe5e45b8c910dd8ec72140d8659039f4001fb
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 01:20:04 +0300

    Добавить константы путей роутинга

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,3 +1,15 @@
+export const PATH_INDEX = '/';
+
 export const PATH_PROFILE = '/profile';
+
+export const PATH_PROFILE_EDIT = `${PATH_PROFILE}/edit`;
+
+export const PATH_PROFILE_PHOTOS = `${PATH_PROFILE}/photos`;
+
+export const PATH_MATCHES = '/matches';
+
+export const PATH_MATCHES_CHAT = (id?: number) => `/matches/chat/${id || ':id'}`;
+
+export const PATH_MATCHES_USER = (id?: number) => `/matches/user/${id || ':id'}`;
 
 export const PATH_RECOMMENDATIONS = '/recommendations';
