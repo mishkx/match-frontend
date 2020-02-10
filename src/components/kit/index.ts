87b266a448643d2aaa218d4fe0b48eda4ece646a
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 02:05:55 +0300

    Добавить общий экспорт kit компонентов

---
+++ src/components/kit/index.ts
@@ -0,0 +1,25 @@
+import BigNavButton from './BigNavButton';
+import ChoiceButtons from './ChoiceButtons';
+import MatchCard from './MatchCard';
+import UserAvatar, { UserChatAvatar, UserMatchedAvatar } from './UserAvatar';
+import UserCard from './UserCard';
+import UserPhotos from './UserPhotos';
+import UserPhotoUpload from './UserPhotoUpload';
+
+export * from './Blocks';
+export * from './Chat';
+export * from './ChatList';
+export * from './Matches';
+export * from './UserCard';
+
+export {
+    BigNavButton,
+    ChoiceButtons,
+    MatchCard,
+    UserAvatar,
+    UserCard,
+    UserChatAvatar,
+    UserMatchedAvatar,
+    UserPhotos,
+    UserPhotoUpload,
+}
