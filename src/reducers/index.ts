e2df5a3dc50ef2781d034165368461249cd5305b
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 01:32:23 +0300

    Рефакторинг корневого редьюсера

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,16 +1,22 @@
 import { connectRouter } from 'connected-react-router';
 import { History } from 'history';
 import { combineReducers } from 'redux';
-import latestChoice from './latestChoice';
-import profile from './profile';
+import chat from './chat';
+import config from './config';
+import lastChoice from './lastChoice';
+import match from './match';
 import recommendation from './recommendation';
 import { AppState } from './types';
+import user from './user';
 
 const createRootReducer = (history: History) => combineReducers<AppState>({
-    latestChoice,
-    profile,
+    chat,
+    config,
+    lastChoice,
+    match,
     recommendation,
     router: connectRouter(history),
+    user,
 });
 
 export default createRootReducer;
