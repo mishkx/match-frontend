77faa54a4a572de4b89c9c3628e833e01c888140
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 01:21:01 +0300

    Обновить константы url-адресов api

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,7 +1,21 @@
-export const URL_PROFILE_INFO = '/api/v1/profile/preferences/info';
+export const URL_APP_CONFIG = '/api/v1/app/config';
 
-export const URL_RECOMMENDATIONS_ITEMS = '/api/v1/recommendation/items';
+export const URL_CHAT = '/api/v1/chats';
 
-export const URL_MATCH_CHOICE_LIKE = '/api/v1/match/choice/like';
+export const URL_CHAT_SINGLE = (id: number) => `${URL_CHAT}/${id}`;
 
-export const URL_MATCH_CHOICE_DISLIKE = '/api/v1/match/choice/dislike';
+export const URL_CHAT_SEND_MESSAGE = (id: number) => `${URL_CHAT}/${id}/message`;
+
+export const URL_CHOICE_DISLIKE = (id: number) => `/api/v1/choice/${id}/dislike`;
+
+export const URL_CHOICE_LIKE = (id: number) => `/api/v1/choice/${id}/like`;
+
+export const URL_MATCH = '/api/v1/matches';
+
+export const URL_MATCH_SINGLE = (id: number) => `${URL_MATCH}/${id}`;
+
+export const URL_RECOMMENDATION_COLLECTION = '/api/v1/recommendations';
+
+export const URL_USER = '/api/v1/user';
+
+export const URL_USER_PHOTO = '/api/v1/user/photo';
