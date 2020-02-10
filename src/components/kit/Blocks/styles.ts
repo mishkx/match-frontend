d08a34aa8ac076e199ec0da74058e4009f36dfe5
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 01:47:49 +0300

    Добавить блок обертки страницы

---
+++ src/components/kit/Blocks/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'src/themes';
+
+export const PageWrapper = styled.div`
+    overflow-y: auto;
+    overflow-x: hidden;
+    padding: 0 ${props => props.theme.all.blockPadding} ${props => props.theme.all.blockPadding} ${props => props.theme.all.blockPadding};
+`;
