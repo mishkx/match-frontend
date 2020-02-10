e095530f7b94e94736917c7684d1b0321a560624
Mikhail Pichushkin <pi4ushkin@gmail.com>
2020-02-11 01:22:13 +0300

    Добавить базовую локализацию en

---
+++ src/i18n/en.ts
@@ -0,0 +1,29 @@
+const en = {
+    translation: {
+        '{{distance}} km from you': '{{distance}} km from you',
+        'Add photo': 'Add photo',
+        Age: 'Age',
+        'Are you sure?': 'Are you sure?',
+        Contact: 'Contact',
+        'Delete match': 'Delete match',
+        Distance: 'Distance',
+        Female: 'Female',
+        'Go to recommendations': 'Go to recommendations',
+        'Go to settings': 'Go to settings',
+        'It\'s a match!': 'It\'s a match!',
+        'Keep searching': 'Keep searching',
+        Male: 'Male',
+        'Page not found': 'Page not found',
+        Photos: 'Photos',
+        Required: 'Required',
+        Save: 'Save',
+        Searching: 'Searching',
+        Settings: 'Settings',
+        'Successfully updated': 'Successfully updated',
+        'There is no one near you': 'There is no one near you',
+        'Try to change your search options': 'Try to change your search options',
+        'Update profile': 'Update profile',
+    },
+};
+
+export default en;
